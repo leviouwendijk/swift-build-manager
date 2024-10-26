e012c423e4681829e42cc6989cf6a4c8e2075867
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2024-10-26 04:09:32 +0200

    Added color extension, without implementation

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,5 +1,40 @@
 import Foundation
 
+enum ANSIColor: String {
+    case reset = "\u{001B}[0m"
+    case bold = "\u{001B}[1m"
+    case dim = "\u{001B}[2m"
+    case italic = "\u{001B}[3m"
+    case underline = "\u{001B}[4m"
+    case black = "\u{001B}[30m"
+    case red = "\u{001B}[31m"
+    case green = "\u{001B}[32m"
+    case yellow = "\u{001B}[33m"
+    case blue = "\u{001B}[34m"
+    case magenta = "\u{001B}[35m"
+    case cyan = "\u{001B}[36m"
+    case white = "\u{001B}[37m"
+    case brightBlack = "\u{001B}[90m" // or gray
+    case brightRed = "\u{001B}[91m"
+    case brightGreen = "\u{001B}[92m"
+    case brightYellow = "\u{001B}[93m"
+    case brightBlue = "\u{001B}[94m"
+    case brightMagenta = "\u{001B}[95m"
+    case brightCyan = "\u{001B}[96m"
+    case brightWhite = "\u{001B}[97m"
+}
+
+protocol StringANSIFormattable {
+    func ansi(_ colors: ANSIColor...) -> String
+}
+
+extension String: StringANSIFormattable {
+    func ansi(_ colors: ANSIColor...) -> String {
+        let colorCodes = colors.map { $0.rawValue }.joined()
+        return "\(colorCodes)\(self)\(ANSIColor.reset.rawValue)"
+    }
+}
+
 enum BuildType: String {
     case debug = "-d"
     case release = "-r"
