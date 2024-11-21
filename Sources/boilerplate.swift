ac2089b039efeae25f28fdf3aba7af4d72414bb8
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2024-11-21 13:21:39 +0100

    added sbm -bin and sbm -bin -detail to see listed views of the sbm-bin dir contents

--- Sources/boilerplate.swift
+++ Sources/boilerplate.swift
@@ -73,3 +73,16 @@ extension String: StringANSIFormattable {
     }
 }
 
+protocol Returnable {
+    mutating func rtn(_ repetitions: Int)
+}
+
+extension String: Returnable {
+    mutating func rtn(_ repetitions: Int = 1) {
+        for _ in 0..<repetitions {
+            self.append("\n")
+        }
+    }
+}
+
+
--- Sources/main.swift
+++ Sources/main.swift
@@ -209,6 +209,97 @@ func cleanBuild(for targetDirectory: String) {
     print("Clean successful!".ansi(.green))
 }
 
+struct Binary {
+    let name: String
+    let path: URL
+    let metadata: [String: Any]? // Dictionary to hold metadata information
+}
+
+func getBinaries() -> [Binary] {
+    var binaries: [Binary] = []
+
+    let sbmBinariesLocation = URL(fileURLWithPath: setupSBMBinDirectory())
+    
+    let fileManager = FileManager.default
+    do {
+        let directoryContents = try fileManager.contentsOfDirectory(at: sbmBinariesLocation, includingPropertiesForKeys: nil, options: [.skipsHiddenFiles])
+        
+        let filenames = Set(directoryContents.map { $0.lastPathComponent })
+        
+        for fileURL in directoryContents {
+            let fileName = fileURL.lastPathComponent
+            guard !fileName.contains(".metadata") else { continue }
+            
+            let metadataFileName = "\(fileName).metadata"
+            if filenames.contains(metadataFileName) {
+                let metadataFileURL = sbmBinariesLocation.appendingPathComponent(metadataFileName)
+                var metadata: [String: String]? = nil
+                
+                // Read and parse plain text metadata
+                if let data = try? String(contentsOf: metadataFileURL) {
+                    var parsedMetadata: [String: String] = [:]
+                    data.split(separator: "\n").forEach { line in
+                        let keyValue = line.split(separator: "=", maxSplits: 1)
+                        if keyValue.count == 2 {
+                            let key = String(keyValue[0]).trimmingCharacters(in: .whitespacesAndNewlines)
+                            let value = String(keyValue[1]).trimmingCharacters(in: .whitespacesAndNewlines)
+                            parsedMetadata[key] = value
+                        }
+                    }
+                    metadata = parsedMetadata
+                } else {
+                    print("Warning: Failed to read metadata file for \(fileName).".ansi(.yellow))
+                }
+                
+                let binary = Binary(name: fileName, path: fileURL, metadata: metadata)
+                binaries.append(binary)
+            }
+        }
+    } catch {
+        print("Error reading contents of directory: \(error)".ansi(.red))
+    }
+    
+    return binaries
+}
+
+enum Presentation {
+    case list
+    case detailed
+}
+
+func hasMetadataFile(_ metadata: [String: Any]?) -> Bool {
+    return metadata != nil && !(metadata?.isEmpty ?? true)
+}
+
+func seeBinaries(_ presentation: Presentation) {
+    let binaries = getBinaries()
+
+    var output = ""
+
+    switch presentation {
+        case .detailed:
+        for binary in binaries {
+            output.append("\(binary.name)".ansi(.bold))
+            output.rtn()
+            output.append("\(binary.path)".ansi(.brightBlack))
+            output.rtn()
+            output.append("\(binary.metadata ?? ["Metadata": "NA"])".ansi(.brightBlack))
+            output.rtn(2)
+        }
+        case .list:
+        for binary in binaries {
+            let binaryString = "\(binary.name)".ansi(.bold)
+            let hasMetadata = hasMetadataFile(binary.metadata)
+            let outputLine = hasMetadata
+                ? binaryString
+                : binaryString + " (missing \(binary.name).metadata)"
+            output.append(outputLine)
+            output.rtn()
+        }
+    }
+    print(output)
+}
+
 func main() {
     print("")
     let arguments = CommandLine.arguments
@@ -226,6 +317,17 @@ func main() {
         case "-clean":
         let projectDirectory = arguments.count > 2 ? arguments[2] : FileManager.default.currentDirectoryPath
         cleanBuild(for: projectDirectory)
+        
+        case "-bin":
+        if arguments.count > 2 {
+            let secondArg = arguments[2]
+
+            if secondArg == "-detail" {
+                seeBinaries(.detailed)
+            }
+        } else {
+            seeBinaries(.list)
+        }
         
         default: 
             guard arguments.count >= 2 else {
